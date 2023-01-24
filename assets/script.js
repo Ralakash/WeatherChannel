b07304cf890dc1c0c6742cad005dd97170258251
Ralakash <shawnkaralash@gmail.com>
2023-01-24 13:52:22 -0500

    adds a bunch of stuff to fix the site and changing the outputs. still having issues with the cards generating not sure if this is a personal computer issue or not though.

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,19 @@
-var savedColleges = [];
-// Grabs the information from the form collegeName and uses it to run the function CollegeScoreCard. Awaits that data then runs schoolCard using a subset of that information
-$('#saveBtn').on('click', async function (event) {
+var savedCitys = [];
+var cityName;
+// Grabs the information from the form cityName and uses it to run the function weatherCard. Awaits that data then runs schoolCard using a subset of that information
+$('#saveBtn').on('click', function (event) {
   event.preventDefault();
-  for (let i = 0; i < cardID.length; i++) {
-    $(cardID[i]).remove();
-    $(buttonID[i]).remove();
-  }
-  let collegeName = $('#collegeName').val();
-  const dataInfo = await collegeScoreCard(collegeName);
-  schoolCard(dataInfo.results);
+  // for (let i = 0; i < cardID.length; i++) {
+  //   $(cardID[i]).remove();
+  //   $(buttonID[i]).remove();
+  // }
+  $('#containerEL').empty;
+  cityName = $('#cityName').val();
+  const dataInfo = weather(cityName);
+  weatherCard(dataInfo);
 });
 
-// Fetches the college information based on the collegeName given above. returns the fetch.
-const collegeScoreCard = (collegeName) => {
-  return fetch(
-    'https://api.data.gov/ed/collegescorecard/v1/schools?school.name=' +
-      collegeName +
-      '&api_key=DoaHn0fsthkrMipg5cL4hkdLpAWcI407Je2idkJN'
-  ).then((res) => res.json());
-};
-
-// Fetches the weather based on the city name generated in schoolCard.
+// Fetches the weather based on the city name generated in the form #cityName.
 const weather = (city) => {
   const openweathermapKey = '99f3a7e5c08aca58214a5ff5a0495fd1';
   return fetch(
@@ -28,68 +21,62 @@ const weather = (city) => {
   ).then((res) => res.json());
 };
 
-// Creates a card element with school and weather data after retrieving information from weather.
+// Creates a card element with weather data after retrieving information from weather.
 let cardID = [];
 let cardBodyID = [];
 let buttonID = [];
-const schoolCard = async (resultsInfo) => {
-  for (let i = 0; i < resultsInfo.length; i++) {
-    let schoolInfo = resultsInfo[i].school;
-    let city = schoolInfo.city;
-    if (!schoolInfo.school_url.startsWith('https://') && schoolInfo.school_url != '') {
-      schoolInfo.school_url = 'https://' + schoolInfo.school_url;
-    }
+const weatherCard = async (resultsInfo) => {
+  let weather1 = [];
+  await resultsInfo.then((weatherData) => {
+    weather1 = weatherData.list;
+    return weather1;
+  });
+  debugger;
+  console.log(weather1[1].main.temp);
+  console.log(weather1.length);
+  for (let i = 0; i < weather1.length; i + 8) {
     cardID[i] = '#card' + i;
     cardBodyID[i] = '#card-body' + i;
     buttonID[i] = '#saveButton' + i;
-    weather(city).then((weatherData) => {
-      let weather1;
-      weather1 = weatherData.list[4].main.temp;
-      // SAM 2: you can also edit these classes on line 41 and 42 to edit to get the buttons in line.
-      // changed style="margin : 0.5rem 0rem;" to mt-2 to follow bootstrap formatting - Sam
-      $('#containerEL').append(
-        '<div class="card col-8 row container-fluid d-flex flex-row mt-2" id="card' + i + '"></div>'
-      );
-      $(cardID[i]).append('<div class="card-body col-8 container-fluid" id="card-body' + i + '"></div>');
-      $(cardBodyID[i]).html(
-        'College name: ' +
-          schoolInfo.name +
-          '&emsp;  &emsp;  City: ' +
-          schoolInfo.city +
-          '<br/> School URL: <a href="' +
-          schoolInfo.school_url +
-          '"target="_blank">' +
-          schoolInfo.school_url +
-          '</a> &emsp;  &emsp;  Temperature(F): ' +
-          weather1 +
-          '°'
-      );
-      console.log(schoolInfo.school_url);
-      // creates a button element that is used to save data to local storage.
-      let button = $('<button type="button" class="btn btn-secondary col-2 my-2" id=saveButton' + i + '>Save</button>');
-      $(cardID[i]).append(button);
-      let schoolObject = {
-        name: schoolInfo.name,
-        city: schoolInfo.city,
-        url: schoolInfo.school_url,
-        temp: weather1,
-      };
-      // creates a sidebar that lists saved colleges, on a click function.
-      $(button).click(function (event) {
-        savedColleges.push(schoolObject);
-        writeToSave();
-        savedColleges = [];
-      });
+
+    $('#containerEL').append(
+      '<div class="card col-8 row container-fluid d-flex flex-row mt-2" id="card' + i + '"></div>'
+    );
+    $(cardID[i]).append('<div class="card-body col-8 container-fluid" id="card-body' + i + '"></div>');
+    $(cardBodyID[i]).html(
+      'City: ' +
+        cityName +
+        '<br/> Date and Time: ' +
+        weather1[i].dt_txt +
+        '<br/> Temperature(F): ' +
+        weather1[i].main.temp +
+        '° <br/> Wind Speed: ' +
+        weather1[i].wind.speed +
+        '<br/> Humidity: ' +
+        weather1[i].main.humidity
+    );
+
+    // creates a button element that is used to save data to local storage.
+    let button = $('<button type="button" class="btn btn-secondary col-2 my-2" id=saveButton' + i + '>Save</button>');
+    $(cardID[i]).append(button);
+    let cityObject = {
+      name: cityName,
+    };
+    // creates a sidebar that lists saved colleges, on a click function.
+    $(button).click(function (event) {
+      savedCitys.push(cityObject);
+      writeToSave();
+      savedCitys = [];
     });
   }
 };
 
 const writeToSave = () => {
   if (JSON.parse(localStorage.getItem('colleges')) != null) {
-    savedColleges = savedColleges.concat(JSON.parse(localStorage.getItem('colleges')));
+    savedCitys = savedCitys.concat(JSON.parse(localStorage.getItem('colleges')));
   }
   // searches the local storage for repeat elements and removes them to avoid duplicates.
-  savedColleges = savedColleges.reduce((finalArray, current) => {
+  savedCitys = savedCitys.reduce((finalArray, current) => {
     let obj = finalArray.find((item) => item.name === current.name);
     if (obj) {
       return finalArray;
@@ -97,38 +84,36 @@ const writeToSave = () => {
       return finalArray.concat([current]);
     }
   }, []);
-  localStorage.setItem('colleges', JSON.stringify(savedColleges));
+  localStorage.setItem('colleges', JSON.stringify(savedCitys));
   $('#sideBar').empty();
-  for (let i = 0; i < savedColleges.length; i++) {
+  for (let i = 0; i < savedCitys.length; i++) {
     let savedBodyID = [];
     let savedID = [];
     savedID[i] = '#saved' + i;
     savedBodyID[i] = '#saved-body' + i;
-    // SAM or ERIC: Please work on using the classes and bootstrap 5 to get the cards here to look better. I will rearrange what data is being fed into them later
-    //              so just focus on ensuring whatever data inside of them is able to be seen appropriately.
     $('#sideBar').append('<div class="card col-9 row mb-2"  id="saved' + i + '"></div>');
     let closeBtn = $(
       '<button type="button" class="btn btn-outline-danger btn-sm col-2 align-self-end" id="close' + i + '">X</button>'
     );
     $(closeBtn).click(function (event) {
-      savedColleges = savedColleges.concat(JSON.parse(localStorage.getItem('colleges')));
-      savedColleges.splice(i, 1);
-      localStorage.setItem('colleges', JSON.stringify(savedColleges));
+      savedCitys = savedCitys.concat(JSON.parse(localStorage.getItem('colleges')));
+      savedCitys.splice(i, 1);
+      localStorage.setItem('colleges', JSON.stringify(savedCitys));
       writeToSave();
     });
     $(savedID[i]).append(closeBtn);
     $(savedID[i]).append('<div class="card-body col-10" id="saved-body' + i + '"></div>');
     $(savedBodyID[i]).html(
       '<a href="' +
-        savedColleges[i].url +
+        savedCitys[i].url +
         '" target="_blank">' +
-        savedColleges[i].name +
+        savedCitys[i].name +
         '</a><br/> ' +
-        savedColleges[i].temp +
+        savedCitys[i].temp +
         '°'
     );
-    console.log(savedColleges[i]);
+    console.log('this is safved citys' + savedCitys[i]);
   }
-  savedColleges = [];
+  savedCitys = [];
 };
 writeToSave();
